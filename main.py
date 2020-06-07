69231b14f7ffbfcf1b1582431b24ca5dcd301150
May tal <may.tal@mail.huji.ac.il>
2020-06-07 15:54:07 +0300

    add functions to textNormalization.py remove duplicates characters and translate slang- work well stemmer_and_lemmatizer - dosent work

--- main.py
+++ main.py
@@ -6,12 +6,8 @@ PATH = 'Data/Data/clean'
 
 def main():
     d = read_data('sentences.neg', PATH, '1')
-    #ts.plot_top_non_stop_words_barchart(d)
-    # for idx, row in d.iterrows():
-    #     ts.print_yap_analysis(row["text"])
-    #     break
-    # tn.stemmer_and_lemmatizer(d, 'out')
-    tn.remove_duplicates_charcters(d)
+    d = tn.text_normalization(d)
+    print("hi")
 
 if __name__ == "__main__":
     main()

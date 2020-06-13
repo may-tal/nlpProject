cdee56529c2deb76723c23b8e4a57822fc13e100
May tal <may.tal@mail.huji.ac.il>
2020-06-13 19:40:44 +0300

    add function that call all the statistic method

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import textNormalization as tn
 PATH = 'Data/Data/clean'
 
 def main():
-    # d = read_data(['sentences.neg', 'sentences.pos'], PATH)
+    d = read_data(['sentences.neg', 'sentences.pos'], PATH)
 
     # text normalization (get the clean data)
     # d_norm = tn.text_normalization(d)
     # d_norm.to_csv('d_norm.csv', index=False, encoding='utf-8')
     d_norm = pd.read_csv('data_to_morphem_by_yap.csv')
 
+    # text statistics
+    ts.get_text_statistics(d)
+
     # classified data
-    classifiers.get_all_classifiers_evaluations(d_norm)
+    # classifiers.get_all_classifiers_evaluations(d_norm)
 
 if __name__ == "__main__":
     main()

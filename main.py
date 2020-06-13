7cca4009a7402ed45d3c5e75d58f914ed1226a99
yardenTal1 <yardental55@gmail.com>
2020-06-13 11:54:47 +0300

    run text normalization

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     # x_train_tf, x_test_tfidf = classifiers.tf_idf(x_train_counts, x_test_counts)
     # classifiers.multinomial_naive_bayes_classifier(x_train_tf, train_df, x_test_tfidf)
 
-    d_norm = tn.text_normalization(d)[:3]
+    d_norm = tn.text_normalization(d)
     d_norm.to_csv('d_norm.csv', index=False, encoding='utf-8')
 
 if __name__ == "__main__":

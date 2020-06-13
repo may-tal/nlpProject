6965babadde9ef0949c92302c41e345a8cb9b734
May tal <may.tal@mail.huji.ac.il>
2020-06-13 12:04:10 +0300

    add just some empty line

--- textNormalization.py
+++ textNormalization.py
@@ -9,14 +9,16 @@ SLANG_DICT = {"אחשלי": "אח שלי", "אמשך": "אמא שלך", "אבש�
               "היפשלי": "היפה שלי", "בצפר": "בית ספר", "כוסעמקערס": "כוס עמק ערס", "אנלא": "אני לא",
               "אנילא": "אני לא", "אמאשך": "אמא שלך", "אבאשך": "אבא שלך"}
 
+
 def text_normalization(data):
     data = remove_duplicates_characters(data)
     data = translate_slang(data)
-
     data = stemmer_and_lemmatizer(data)
     return data
 
+
 def stemmer_and_lemmatizer(data):
+
     for idx, row in data.iterrows():
         yap_df = ts.get_yap_analysis(row["text"])
         lemme = yap_df["lemma"].values
